44f47c25f0f8a1cbd658e30933fd2631807c0ddc
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-04-25 22:43:56 +0000

    refactor: ensure controler just handle request

---
+++ api/__tests__/modules/controllers/createTransactionsFromCsv.test.js
@@ -0,0 +1,52 @@
+const { describe, expect, it } = require('@jest/globals')
+const request = require('supertest')
+const path = require('path')
+
+const app = require('../../../config/server')
+const mongoHelper = require('../../../src/repository-util/mongodb-setup')
+const env = require('../../../config/env')
+const FIXTURE_PATH = '../../fixtures/csv_files/'
+
+const transactions = [
+  {
+    bankOrigin: 'BANCO DO BRASIL',
+    agencyOrigin: '0001',
+    accountOrigin: '00001-1',
+    bankDestiny: 'BANCO BRADESCO',
+    agencyDestiny: '0001',
+    accountDestiny: '00001-1',
+    amount: 8000,
+    date: new Date('2022-01-01T16:30:00')
+  }
+]
+
+describe('Create Transactions From CSV', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(env.mongoUrl)
+  })
+  beforeEach(async () => {
+    const collection = await mongoHelper.getCollection('transaction')
+    await collection.deleteMany({})
+  })
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+  it.only('ensure create transactions returns 200 if files ok', async () => {
+    const csvFile = path.resolve(__dirname, FIXTURE_PATH, 'ok-file.csv')
+    const response = await request(app).post('/history').attach('history', csvFile)
+    expect(response.status).toBe(200)
+  })
+  it('ensure create transactions returns 400 if already exists transaction for the day', async () => {
+    const collection = await mongoHelper.getCollection('transaction')
+    await collection.insertOne(transactions[0])
+    const csvFile = path.resolve(__dirname, FIXTURE_PATH, 'ok-file.csv')
+    const response = await request(app).post('/history').attach('history', csvFile)
+    expect(response.status).toBe(200)
+  })
+
+  it('ensure should return 400 if file is empty', async () => {
+    const csvFile = path.resolve(__dirname, FIXTURE_PATH, 'empty.csv')
+    const response = await request(app).post('/history').attach('history', csvFile)
+    expect(response.status).toBe(400)
+  })
+})
--- api/src/modules/transactions/controllers/createTransactionsFromCsv.js
+++ api/src/modules/transactions/controllers/createTransactionsFromCsv.js
@@ -1,26 +1,17 @@
-const { promises, createReadStream } = require('fs')
-const { parse } = require('csv-parse')
-module.exports = function createTransactionsFromCsv() {
-  async function loadRegisters(file) {
-    return new Promise((resolve, reject) => {
-      const registros = []
-      const stream = createReadStream(file);
-      const parseFile = parse()
-      stream.pipe(parseFile)
-      parseFile.on('data', line => {
-        console.log(line)
-      }).on("end", () => {
-        promises.unlink(file);
-        resolve(registros);
-      })
-        .on("error", (err) => {
-          reject(err);
-        });
-    })
-  }
-  return handle = async (request, response) => {
-    const file = request.file.path
-    await loadRegisters(file)
-    response.json({ message: 'your file was received' })
+
+module.exports = function createTransactionsFromCsv (createTransactionsService) {
+  return async (request, response) => {
+    try {
+      const filePath = request.file.path
+      await createTransactionsService({ filePath })
+      response.json({ message: 'your file was received' })
+    } catch (error) {
+      console.log(error.message)
+      let status = 500
+      if (error.name === 'emptyFile' && error.name === 'transactionAlreadyExists') {
+        status = 400
+      }
+      return response.status(status).json({ message: error.message })
+    }
   }
 }
